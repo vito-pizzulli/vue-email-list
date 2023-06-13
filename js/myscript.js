e796b888b833f8c8a2c78190fd3889ba413804bf
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-13 15:14:55 +0200

    fixed code indentation

--- js/myscript.js
+++ js/myscript.js
@@ -9,9 +9,9 @@ createApp ({
     created() {
         for (i = 0; i < 10; i++) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then((response) => {
-            const result = response.data;
-            this.mailList.push(result.response);
+            .then((response) => {
+                const result = response.data;
+                this.mailList.push(result.response);
             });
         }
     }
