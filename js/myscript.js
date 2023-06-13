7cf49145c2ba9cfec75027842a35ff71074adc6c
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-13 15:59:22 +0200

    implemented user choice of the n of generated mail

--- js/myscript.js
+++ js/myscript.js
@@ -3,16 +3,22 @@ const { createApp } = Vue;
 createApp ({
     data() {
         return {
-            mailList: []
+            mailList: [],
+            mailNumberInput: '',
+            mailNumber: ''
         }
     },
-    created() {
-        for (i = 0; i < 10; i++) {
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then((response) => {
-                const result = response.data;
-                this.mailList.push(result.response);
-            });
+    methods: {
+        getMailList() {
+            this.mailNumber = this.mailNumberInput;
+            this.mailList = [];
+            for (i = 0; i < this.mailNumber; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    const result = response.data;
+                    this.mailList.push(result.response);
+                });
+            }
         }
     }
 }) .mount('#app');
